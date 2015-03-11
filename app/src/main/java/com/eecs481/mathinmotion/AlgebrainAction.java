42a742ea9894ea01f46214f01cb3caba932a7ced
newsteinleo <jiangp@umich.edu>
2015-03-11 16:26:47 -0400

    we have math problems

--- app/src/main/java/com/eecs481/mathinmotion/AlgebrainAction.java
+++ app/src/main/java/com/eecs481/mathinmotion/AlgebrainAction.java
@@ -11,10 +11,15 @@ import android.widget.LinearLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Random;
+
 
 public class AlgebrainAction  extends ActionBarActivity implements MotionListener {
     LinearLayout mLinearLayout;
     String answer = "";
+    String questionFormat = "equations"; // can be "addition", "multiplication", etc
+    String answerLine;
+    String question;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +33,8 @@ public class AlgebrainAction  extends ActionBarActivity implements MotionListene
         // Add the ImageView to the layout and set the layout as the content view
         mLinearLayout.addView(i);
         setContentView(R.layout.activity_algebrain_action);
-        TextView current = (TextView) findViewById(R.id.answer_display);
-        current.setText("");
+
+        generateProblem();
     }
     public void clicker(View view)
     {
@@ -39,15 +44,59 @@ public class AlgebrainAction  extends ActionBarActivity implements MotionListene
             append(name);
         }
     }
+    public void generateProblem()
+    {
+        Random randomGenerator = new Random();
+        randomGenerator.setSeed(randomGenerator.nextLong());
+        if(questionFormat.equals("addition"))
+        {
+            int a = randomGenerator.nextInt(100);
+            int b = randomGenerator.nextInt(100);
+            question = Integer.toString(a) + " + " + Integer.toString(b) + "=";
+            answerLine = Integer.toString(a+b);
+        }
+        else if(questionFormat.equals("multiplication"))
+        {
+
+            int a = randomGenerator.nextInt(30);
+            int b = randomGenerator.nextInt(30);
+            question = Integer.toString(a) + " x " + Integer.toString(b) + "=";
+            answerLine = Integer.toString(a*b);
+        }
+        else if(questionFormat.equals("equations"))
+        {
+            int a = randomGenerator.nextInt(100);
+            int b = randomGenerator.nextInt(20);
+            int c = randomGenerator.nextInt(100);
+            question = "Find x: "+ Integer.toString(b) + "x + "+Integer.toString(c) + " = " + Integer.toString(a*b +c);
+            answerLine = Integer.toString(a);
+        }
+        TextView current = (TextView) findViewById(R.id.problem_display);
+        current.setText(question);
+        current = (TextView) findViewById(R.id.answer_display);
+        current.setText("");
+        current.setBackgroundColor(0);
+        answer = "";
+    }
     public void append(String digit)
     {
         answer = answer + digit;
         TextView current = (TextView) findViewById(R.id.answer_display);
         current.setText(answer);
     }
+
     public void submit(View view)
     {
-
+        TextView current = (TextView) findViewById(R.id.answer_display);
+        String userAnswer = current.getText().toString();
+        if(userAnswer.equals(answerLine))
+        {
+            generateProblem();
+        }
+        else
+        {
+            current.setBackgroundColor(-65536);
+        }
     }
     public void bksp (View view)
     {
@@ -62,6 +111,9 @@ public class AlgebrainAction  extends ActionBarActivity implements MotionListene
     protected void onResume()
     {
         super.onResume();
+        if (Motion.getInstance() != null) {
+            return;
+        }
         Motion.getInstance().addListener(this, this);
 
     }
@@ -69,6 +121,9 @@ public class AlgebrainAction  extends ActionBarActivity implements MotionListene
     protected void onPause()
     {
         super.onPause();
+        if (Motion.getInstance() != null) {
+            return;
+        }
         Motion.getInstance().addListener(this, this);
     }
     @Override
